fe829a9fffa4b770097c9dc9dd677ffcf394287c
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-08-15 21:12:02 -0300

    Create exercicio1.java

---
+++ exercicio1.java
@@ -0,0 +1,19 @@
+/**
+Comentário em documentação = o que o código faz e o autor :)
+@author AnnaLauraCsS
+o ln no final do PRINLN faz a QUEBRA DE LINHA 
+O COMANDO System.out.println mostra uma mensagem no terminal
+As linhas de código terminam com ;
+Java segue o sintaxe camal case -> EuSouProgramadora NomeDaVariavel
+o nome da classe tem que ser o nome do arquivo
+    Ex: public class App (nome do arquivo = App.java)
+O publico static void main é uma instrução e tudo que estiver dentro dele será executado de forma sequencial
+Variáveis -> Armazenar dados 
+Métodos ou Funções -> são blocos de códigos reutilizáveis para alterar dados 
+ */
+
+public class App{
+    public static void main(String[] args){
+        System.out.println("OI SARINHA, EU TE AMO!");
+    }
+}
