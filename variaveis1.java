88148d073f40521ac3112c6d50fccd7f999b0c66
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-13 21:57:40 -0300

    Create variaveis1.java

---
+++ variaveis1.java
@@ -0,0 +1,15 @@
+public class variaveisEmJava {
+    
+    public static void main(String[] args){
+        int idade = 10; 
+        double preco = 10.334;
+        char genero = 'F'; 
+        boolean maiorIdade = true;
+
+        System.out.println(idade);
+        System.out.println(preco); 
+        System.out.println(genero); 
+        System.out.println(maiorIdade);
+
+    }
+}
