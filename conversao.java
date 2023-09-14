86e1fb1166a7be35ae0b01d978a5b493e6185200
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-13 22:00:28 -0300

    Create conversao.java

---
+++ conversao.java
@@ -0,0 +1,10 @@
+public class conversao {
+    
+    //conversão de tipos de variáveis
+    public static void main(String[] args){
+        int x = 10; 
+        double y = (int) x; 
+
+        System.out.println(y);
+    }
+}
