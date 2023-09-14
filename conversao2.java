36ba49b24717e365d7ab55933fa3bb21923ca8cf
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-13 22:06:41 -0300

    Create conversao2.java

---
+++ conversao2.java
@@ -0,0 +1,12 @@
+public class conversao2 {
+
+    //conversão de double para inteiro @author = annalaura2
+    public static void main(String[]args){
+
+        double valor = 9.7890; 
+        int valorConvertido = (int) valor;
+
+        System.out.println(valorConvertido);
+    }
+    
+}
