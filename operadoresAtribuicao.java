7b4b1cc8dd9cf194922009cbbfd679220fbd01c9
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-15 21:40:32 -0300

    Create operadoresAtribuicao.java

---
+++ operadoresAtribuicao.java
@@ -0,0 +1,20 @@
+public class operacoesDeAtribuicao {
+
+    public static void main(String[] args) {
+        int x = 10;
+        x = x + 15; // uma forma de fazer atribuição de valores
+
+        System.out.println(x);
+
+        int y = 24;
+        y += 20; // outra de forma de fazer atribuição, é possível fazer das duas maneiras com os
+                 // operadores
+        System.out.println(y); // aritméticos, menos o módulo (%)
+
+        int z = 2;
+        z *= 4;
+
+        System.out.println(z);
+
+    }
+}
