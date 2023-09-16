db45975fab2241266e3184ffcc2ab1bcd6404e9b
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-15 21:43:20 -0300

    Create incrementoEDecremento.java

---
+++ incrementoEDecremento.java
@@ -0,0 +1,15 @@
+public class decrementoEIncremento {
+
+    public static void main(String[] args) {
+        int x = 10;
+
+        x++; // a mesma coisa de x = x + 1;
+
+        System.out.println(x);
+
+        x--; // a mesma coisa de x = x - 1;
+
+        System.out.println(x);
+    }
+
+}
