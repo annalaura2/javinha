91c7e980602e5565c8e85d305ac743e6cea5692d
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-15 21:49:58 -0300

    Create relacionaisOuIgualdade.java

---
+++ relacionaisOuIgualdade.java
@@ -0,0 +1,20 @@
+public class operadoresDeIgualdadeERelacionais {
+
+    public static void main(String[] args) {
+        int idade = 10;
+
+        boolean maiorIdade = idade > 18;
+
+        System.out.println(maiorIdade);
+
+        int z = 25;
+        int h = 100;
+        boolean maiorNumero = z > h;
+        System.out.println(maiorNumero);
+
+        boolean menorNumero = h > z;
+        System.out.println(menorNumero);
+
+    }
+
+}
