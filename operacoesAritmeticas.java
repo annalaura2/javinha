c3b52e7a9cd995a2f341820f05410fe9e64131e0
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-15 21:32:09 -0300

    Create operacoesAritmeticas.java

---
+++ operacoesAritmeticas.java
@@ -0,0 +1,25 @@
+public class operacoesAritmeticas {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 7;
+        int z = 4;
+
+        int divisao = y % x;
+        System.out.println(divisao);
+
+        int divisao2 = y / x;
+        System.out.println(divisao2);
+
+        int multiplicacao = y * x;
+        System.out.println(multiplicacao);
+
+        int adicao = y + x;
+        System.out.println(adicao);
+
+        int expressao = (x * y) + z;
+        System.out.println(expressao);
+
+        int expressao2 = y / x * z;
+        System.out.println(expressao2);
+    }
