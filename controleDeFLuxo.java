16c651a1c28323dd69fcfec1c30e443b81d6f4ae
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-25 23:06:44 -0300

    Create controleDeFLuxo.java

---
+++ controleDeFLuxo.java
@@ -0,0 +1,13 @@
+public class ifEelse {
+
+    public static void main(String[] args) {
+        int idade = 16;
+
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Não é maior de idade");
+        }
+    }
+
+}
