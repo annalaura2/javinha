ae9031fff0337c3db3b9b11245024705e9df770f
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-09-27 09:26:51 -0300

    Create exercicio2.java

---
+++ exercicio2.java
@@ -0,0 +1,12 @@
+package cursoDeJava;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+
+	 public static void main(String[] args) {
+	        System.out.println("Digite um número: ");
+	        Scanner in = new Scanner( System.in );
+	        System.out.println(in.nextLine());
+	    }
+}
