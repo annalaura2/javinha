3081172e60cb9270d48e243f527b3d70c70e722a
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-10-06 21:36:50 -0300

    Create leituraDeDados1.java

---
+++ leituraDeDados1.java
@@ -0,0 +1,16 @@
+package cursoDeJava;
+
+import java.util.Scanner;
+
+public class exercicio2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe a idade: ");
+		int idade = sc.nextInt(); 
+		
+		sc.close();
+	}
+}
