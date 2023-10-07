448b626ea1e12dbf944d9ad7dd9255b52c97849a
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-10-06 22:34:51 -0300

    Create testeConta.java

---
+++ testeConta.java
@@ -0,0 +1,12 @@
+package comercialBanco.java;
+
+public class testeConta {
+	
+	public static void main(String[] args) {
+		
+		Conta contaCorrente = new Conta();
+		Conta contaPoupanca = new Conta();
+	}
+
+}
+
