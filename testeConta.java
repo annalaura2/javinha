a1bf68e7f1a2e629f84f4b8a73e99ba1c5282ab0
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-10-22 22:09:47 -0300

    Update testeConta.java

--- testeConta.java
+++ testeConta.java
@@ -9,13 +9,20 @@ public class testeConta {
 		Conta contaInvestimento = new Conta();
 		
 		contaCorrente.numero = 11; 
-		contaCorrente.saldo = 20.5; 
+		contaCorrente.saldo = 20.5;
+		contaCorrente.cliente.nome = "Sarah"; 
+		contaCorrente.cliente.idade = 20; 
 		
 		contaPoupanca.numero = 22; 
 		contaPoupanca.saldo = 10.4; 
+		contaPoupanca.cliente.nome = "Anna"; 
+		contaPoupanca.cliente.idade = 19;
+	
 		
 		contaInvestimento.numero = 33; 
 		contaInvestimento.saldo = 45.6; 
+		contaInvestimento.cliente.nome = "Eva"; 
+		contaInvestimento.cliente.idade = 9; 
 	}
 
 }
