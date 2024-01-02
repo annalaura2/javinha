51831b5ccb987f378c450c1d0b097ee2d822c0e5
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-02 17:40:11 -0300

    Create atribuicao.java

---
+++ atribuicao.java
@@ -0,0 +1,24 @@
+package cursoDeJava;
+
+public class atribuicao {
+	
+	public static void main(String[] args) {
+		
+		int idade = 19;  // atribuição
+		System.out.println("Idade: " + idade);
+		
+		idade += 10; // operador matemático
+		System.out.println("Idade: " + idade);
+		
+		int a = 30; 
+		int b = 40; 
+		
+		if(a > b) {
+			System.out.println("A é maior");
+		}else {
+			System.out.println("B é maior");
+		}
+		
+	}
+
+}
